cf45b38989ba8ea828b7ef70a2bfbea76993a15e
Yuchen Yuan <ycyuan@bu.edu>
2022-07-11 17:06:17 -0400

    debug evaluation code

--- multifree/nn/aae.py
+++ multifree/nn/aae.py
@@ -94,6 +94,7 @@ class AAE(nn.Module):
         self.loss_g_train = {}
         self.loss_g_val = {}
         self.loss_d_train = []
+        self.loss_d_val = []
         
         self.z_real = torch.ones((self.params['batchsize'], 1)).to(self.params['device'])
         self.z_fake = torch.zeros((self.params['batchsize'], 1)).to(self.params['device'])
@@ -122,7 +123,7 @@ class AAE(nn.Module):
         assert isinstance(other_args, tuple)
         return x_hat, other_args
     
-    def train_discriminator(self, z: torch.Tensor, noise: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def train_discriminator(self, z: torch.Tensor, noise: torch.Tensor, z_real: torch.Tensor=None, z_fake: torch.Tensor=None) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         """
         Train the discriminator.
         
@@ -141,11 +142,19 @@ class AAE(nn.Module):
             The prediction of the discriminator for the latent space from the encoder
         loss_d : torch.Tensor
             The discriminator loss value
+        z_real : torch.Tensor
+            The real latent space
+        z_fake : torch.Tensor
+            The fake latent space
         """
+        if z_real is None:
+            z_real = self.z_real
+        if z_fake is None:
+            z_fake = self.z_fake
         target_real = self.discriminator(noise)
         target_generator = self.discriminator(z.detach())
-        real_loss = self.criterion_D(target_real, self.z_real)
-        fake_loss = self.criterion_D(target_generator, self.z_fake)
+        real_loss = self.criterion_D(target_real, z_real)
+        fake_loss = self.criterion_D(target_generator, z_fake)
         loss_d = 0.5 * (real_loss + fake_loss)
         return target_real, target_generator, loss_d
     
@@ -186,7 +195,55 @@ class AAE(nn.Module):
         """
         for i in range(self.params['nepoch']):
             self._train_one_epoch(i, train_data, val_data)
-    
+   
+    def _validate_after_one_epoch(self, val_data: torch.Tensor) -> tuple[dict, torch.Tensor]:
+        """
+        A private method to run on validation set after  one epoch.
+        
+        Parameters
+        ----------
+        val_data : 
+            The validation set to be run at this step
+            
+        Returns
+        -------
+        loss_g_dict : dict
+            A dictionary to store each component of the generator loss
+        loss_d : torch.Tensor
+            The discriminator loss
+        """
+        self.generator.eval()
+        self.discriminator.eval()
+        val_z_real = torch.ones((len(val_data[0]), 1)).to(self.params['device'])
+        val_z_fake = torch.zeros((len(val_data[0]), 1)).to(self.params['device'])
+        # In case we feed a labeled dataset into the model, we only select the training data but not the labels
+        if isinstance(val_data, list):
+            x_batch = val_data[0].to(self.params['device'])
+        else:
+            x_batch = val_data.to(self.params['device'])
+        if self.whitening:
+            x = self.pcawhitening(x_batch)
+            assert x.shape[0] == x_batch.shape[0]
+            assert x.shape[1] == x_batch.shape[1] - self.params['dof']
+        else:
+            x = x_batch
+            assert x.shape == x_batch.shape
+        # Train the generator
+        x_hat, generator_args = self.train_generator(x)
+        z = generator_args[0]
+        if self.whitening:
+            x_rec = self.pcaunwhitening(x_hat)
+
+        # Compute generator loss
+        loss_g_dict = self.criterion_G(x_batch, x_rec, generator_args[1:], self.params['beta'])
+        loss_g = 0.999 * loss_g_dict['loss'] + 0.001 * self.criterion_D(self.discriminator(z), val_z_real)
+
+        # Train the discriminator
+        noise = self.true_prior.sample(sample_shape=torch.tensor([len(z),
+                                       self.params['latent']])).to(self.params['device'])
+        target_real, target_generator, loss_d = self.train_discriminator(z, noise, z_real=val_z_real, z_fake=val_z_fake)
+        return loss_g_dict, loss_d
+ 
     def _train_one_epoch(self, i: int, train_data: torch.utils.data.DataLoader, val_data: torch.utils.data.DataLoader=None) -> None:
         """
         A private method to train one epoch of all training data.
@@ -204,6 +261,8 @@ class AAE(nn.Module):
         -------
         None
         """
+        self.generator.train()
+        self.discriminator.train()
         # Enumerate each minibatch training data
         for j, train_batch in enumerate(train_data):
             loss_g_dict, loss_d = self._train_one_step(j, train_batch)
@@ -215,7 +274,6 @@ class AAE(nn.Module):
         if i == 0:
             for k in keys:
                 self.loss_g_train[k] = [loss_g_dict[k].item()]
-                self.loss_g_val[k] = []
         else:
             for k in keys:
                 self.loss_g_train[k].append(loss_g_dict[k].item())
@@ -229,7 +287,25 @@ class AAE(nn.Module):
                 report += '{0:s}: {1:f} '.format(k, loss_g_dict[k].item())
             report = report[:-1] + '; Discriminator loss {0:f}'.format(loss_d)
             print(report[:-1])
-   
+  
+        # If val data is provided
+        if val_data is not None:
+            loss_g_dict, loss_d = self._validate_after_one_epoch(val_data)
+            # Record loss after each epoch
+            if i == 0:
+                for k in keys:
+                    self.loss_g_val[k] = [loss_g_dict[k].item()]
+            else:
+                for k in keys:
+                    self.loss_g_val[k].append(loss_g_dict[k].item())
+            self.loss_d_val.append(loss_d.item())
+            if self.verbose:
+                report = 'VAL LOSS - Generator '
+                for k in keys:
+                    report += '{0:s}: {1:f} '.format(k, loss_g_dict[k].item())
+                report = report[:-1] + '; Discriminator loss {0:f}'.format(loss_d)
+                print(report[:-1])
+        
         # Update scheduler after each epoch
         if self.scheduler_G is not None:
             self.scheduler_G.step()
@@ -327,7 +403,60 @@ class SupervisedAAE(AAE):
             label = label.int()
             labels_one_hot[k, label] = 1
         return labels_one_hot
-                
+               
+    def _validate_after_one_epoch(self, val_data: torch.Tensor) -> tuple[dict, torch.Tensor]:
+        """
+        A private method to run on validation set after  one epoch.
+        
+        Parameters
+        ----------
+        val_data : 
+            The validation set to be run at this step
+            
+        Returns
+        -------
+        loss_g_dict : dict
+            A dictionary to store each component of the generator loss
+        loss_d : torch.Tensor
+            The discriminator loss
+        """
+        self.generator.eval()
+        self.discriminator.eval()
+        val_z_real = torch.ones((len(val_data[0]), 1)).to(self.params['device'])
+        val_z_fake = torch.zeros((len(val_data[0]), 1)).to(self.params['device'])
+
+        x_batch = val_data[0].to(self.params['device'])
+        label_batch = val_data[1].to(self.params['device'])
+        labels_one_hot = self._label_to_one_hot_vector(label_batch).to(self.params['device'])
+        if self.whitening:
+            x = self.pcawhitening(x_batch)
+            assert x.shape[0] == x_batch.shape[0]
+            assert x.shape[1] == x_batch.shape[1] - self.params['dof']
+        else:
+            x = x_batch
+            assert x.shape == x_batch.shape
+
+        # Train the generator
+        x_hat, generator_args = self.train_generator(x)
+        z = generator_args[0]
+        z_label_batch = torch.cat((z, labels_one_hot), dim=1)
+
+        if self.whitening:
+            x_rec = self.pcaunwhitening(x_hat)
+        else:
+            x_rec = x_hat
+        
+        # Compute generator loss
+        loss_g_dict = self.criterion_G(x_batch, x_rec, generator_args[1:], self.params['beta'])
+        loss_g = 0.99 * loss_g_dict['loss'] + 0.01 * self.criterion_D(self.discriminator(z_label_batch), val_z_real)
+        # Train the discriminator
+        noise, noise_one_hot_label = self.true_prior.sample(sample_shape=torch.tensor([len(z),
+                                                            self.params['latent']]),
+                                                            return_components=True)
+        noise_label_batch = torch.cat((noise, noise_one_hot_label), dim=1)
+        target_real, target_generator, loss_d = self.train_discriminator(z_label_batch, noise_label_batch, z_real=val_z_real, z_fake=val_z_real)
+        return loss_g_dict, loss_d
+ 
     def _train_one_step(self, j: int, train_batch) -> tuple[torch.Tensor, torch.Tensor]:
         """
         A private method to train one step in an epoch.
@@ -402,6 +531,57 @@ class EnergyGapSAAE(SupervisedAAE):
                                             optim_g, optim_d, generator_loss, discriminator_loss, true_prior,
                                             PCAWhitening, PCAUnWhitening, whitened_loss,
                                             variational, whitening, verbose)
+
+    def _validate_after_one_epoch(self, val_data: torch.Tensor) -> tuple[dict, torch.Tensor]:
+        """
+        A private method to run on validation set after  one epoch.
+        
+        Parameters
+        ----------
+        val_data : 
+            The validation set to be run at this step
+            
+        Returns
+        -------
+        loss_g_dict : dict
+            A dictionary to store each component of the generator loss
+        loss_d : torch.Tensor
+            The discriminator loss
+        """
+        self.generator.eval()
+        self.discriminator.eval()
+        val_z_real = torch.ones((len(val_data[0]), 1)).to(self.params['device'])
+        val_z_fake = torch.zeros((len(val_data[0]), 1)).to(self.params['device'])
+
+        x_batch = val_data[0].to(self.params['device'])
+        energy_batch = val_data[1].to(self.params['device'])
+        label_batch = val_data[2].to(self.params['device'])
+        labels_one_hot = self._label_to_one_hot_vector(label_batch).to(self.params['device'])
+        if self.whitening:
+            x = self.pcawhitening(x_batch)
+            assert x.shape[0] == x_batch.shape[0]
+            assert x.shape[1] == x_batch.shape[1] - self.params['dof']
+        else:
+            x = x_batch
+            assert x.shape == x_batch.shape
+
+        # Train the generator
+        energy_hat, generator_args = self.train_generator(x)
+        z = generator_args[0]
+        z_label_batch = torch.cat((z, labels_one_hot), dim=1)
+
+        # Compute generator loss
+        # Here the decoder outputs the energy gap so we compare the decoder learned
+        # energy gap with the real energy gap
+        loss_g_dict = self.criterion_G(energy_batch, energy_hat, generator_args[1:], self.params['beta'])
+        loss_g = 0.99 * loss_g_dict['loss'] + 0.01 * self.criterion_D(self.discriminator(z_label_batch), val_z_real)
+        # Train the discriminator
+        noise, noise_one_hot_label = self.true_prior.sample(sample_shape=torch.tensor([len(z),
+                                                            self.params['latent']]),
+                                                            return_components=True)
+        noise_label_batch = torch.cat((noise, noise_one_hot_label), dim=1)
+        target_real, target_generator, loss_d = self.train_discriminator(z_label_batch, noise_label_batch, z_real=val_z_real, z_fake=val_z_fake)
+        return loss_g_dict, loss_d
 
     def _train_one_step(self, j: int, train_batch) -> tuple[dict, torch.Tensor]:
         """
